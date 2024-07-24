6b121cc1d0c41e8d455e8c268b36a64e58d8c5c8
daun-up <awhekdns@kookmin.ac.kr>
2024-07-24 18:45:56 +0900

    feat: 회원 관리 예제 - 웹 MVC 개발

---
+++ src/main/java/hello/hello_spring/SpringConfig.java
@@ -0,0 +1,20 @@
+package hello.hello_spring;
+
+import hello.hello_spring.repository.MemberRepository;
+import hello.hello_spring.repository.MemoryMemberRepository;
+import hello.hello_spring.service.MemberService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringConfig {
+    @Bean
+    public MemberService memberService() { // 스프링 빈에 등록
+        return new MemberService(memberRepository());
+    }
+
+    @Bean
+    public MemberRepository memberRepository(){ // MemberRepository 는 인터페이스이고
+        return new MemoryMemberRepository(); // MemoryMemberRepository 는 구현체이다.
+    }
+}
--- src/main/java/hello/hello_spring/service/MemberService.java
+++ src/main/java/hello/hello_spring/service/MemberService.java
@@ -9,10 +9,10 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 
-@Service
+
 public class MemberService {
     private final MemberRepository memberRepository;
-    @Autowired
+
     public MemberService(MemberRepository memberRepository) { // 멤버 리포지토리를 직접 new 해서 생성하는 것이 아니라 외부에서 넣어주게 바꿔줌
         this.memberRepository = memberRepository;
     }
